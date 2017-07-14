4cfffed66b2cd9e8a8b8e28543f47b484079dc49
mdalal2020 <mdalal@berkeley.edu>
2017-07-14 14:42:30 -0700

    updated files

--- scripts/client.py
+++ scripts/client.py
@@ -3,16 +3,17 @@ import rospy
 from robot_info.srv import *
 import numpy as np
 
-def get_jacobian_client():
+def get_jacobian_client(name):
     rospy.wait_for_service('get_jacobian')
     try:
-        get_jacobian = rospy.ServiceProxy('get_jacobian', GetJacobian)
-        resp = get_jacobian()
-        # return resp1.jacobian
+        get_jacobian = rospy.ServiceProxy('get_jacobian', GetJacobian, persistent=True)
+        resp = get_jacobian(name)
         return np.array([resp.jacobianr1, resp.jacobianr2, resp.jacobianr3, resp.jacobianr4, resp.jacobianr5, resp.jacobianr6])
     except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+        return resp
 
 if __name__ == "__main__":
-    angles = np.zeros(7)
-    print(get_jacobian_client())
+    for i in range(10000):
+        # print(get_jacobian_client('left'))
+        resp = get_jacobian_client('left')
+        print(i)
--- scripts/server.py
+++ scripts/server.py
@@ -6,13 +6,19 @@ import numpy as np
 from baxter_pykdl import baxter_kinematics
 
 def handle_get_jacobian(req):
-    kin = baxter_kinematics('right')
+    if req.name == 'right':
+        kin = right_kin
+    else:
+        kin = left_kin
     jacobian = kin.jacobian().getA()
     # return GetJacobianResponse(req.angles)
     return GetJacobianResponse(jacobian[0], jacobian[1], jacobian[2], jacobian[3], jacobian[4], jacobian[5])
-
 def get_jacobian_server():
     rospy.init_node('get_jacobian_server')
+    global right_kin 
+    right_kin = baxter_kinematics('right')
+    global left_kin 
+    left_kin = baxter_kinematics('left')
     s = rospy.Service('get_jacobian', GetJacobian, handle_get_jacobian)
     rospy.spin()
 
