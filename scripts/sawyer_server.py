1050cc95d552da98d4a13981965c075a475c56b7
¨Murtaza <mdalal@berkeley.edu>
2017-07-18 11:12:48 -0700

    fixed sawyer_server.py

--- scripts/sawyer_server.py
+++ scripts/sawyer_server.py
@@ -6,11 +6,12 @@ import numpy as np
 from baxter_pykdl import baxter_kinematics
 
 def handle_get_jacobian(req):
-    kin = baxter_kinematics('right')
     jacobian = kin.jacobian().getA()
     return GetJacobianResponse(jacobian[0], jacobian[1], jacobian[2], jacobian[3], jacobian[4], jacobian[5])
 def get_jacobian_server():
     rospy.init_node('get_jacobian_server')
+    global kin
+    kin = baxter_kinematics('right')
     s = rospy.Service('get_jacobian', GetJacobian, handle_get_jacobian)
     rospy.spin()
 
