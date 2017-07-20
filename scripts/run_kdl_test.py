19ac2224177d3b8ed8c06cdca4c96b29ffa60ce2
mdalal2020 <mdalal@berkeley.edu>
2017-07-19 21:56:41 -0700

    modified robot_info to run with all joint objects

---
+++ scripts/run_kdl_test.py
@@ -0,0 +1,25 @@
+from robot_info.srv import *
+import rospy
+import numpy as np
+from urdf_parser_py.urdf import URDF
+import baxter_interface as bi
+from pykdl_utils.kdl_kinematics import KDLKinematics
+
+rospy.init_node('get_robot_pose_jacobian_server')
+robot = URDF.from_parameter_server(key='robot_description')
+
+kin = KDLKinematics(robot, 'base', 'right_gripper')
+arm = bi.Limb('right')
+
+q = arm.joint_angles()
+q = [q['right' + '_s0'], q['right' + '_s1'], q['right' + '_e0'], q['right' + '_e1'], q['right' + '_w0'],
+     q['right' + '_w1'], q['right' + '_w2']]
+
+end = 'right_upper_shoulder'
+pose = kin.forward(q, end)
+pose = np.squeeze(np.asarray(pose))
+pose = [pose[0][3], pose[1][3], pose[2][3]]
+
+print '\n\n' + str(pose)
+
+print '\n\n' + str(kin.jacobian(q, pose))
--- scripts/sawyer_server.py
+++ scripts/sawyer_server.py
@@ -7,12 +7,16 @@ from urdf_parser_py.urdf import URDF
 import intera_interface as ii
 from pykdl_utils.kdl_kinematics import KDLKinematics
 
+joints = ['right_j0', 'right_j1', 'right_j2', 'right_j3', 'right_j4', 'right_j5', 'right_j6', 'right_hand']
+kin_dict = {}
+
 def handle_get_robot_pose_jacobian(req):
-    arm = ii.Limb(req.name)
     q = arm.joint_angles()
     q = [q[req.name + '_j0'], q[req.name + '_j1'], q[req.name + '_j2'], q[req.name + '_j3'], q[req.name + '_j4'],
          q[req.name + '_j5'], q[req.name + '_j6']]
-    pose = kin.forward(q, end_link=req.name + req.tip)
+    joint = req.name + req.tip
+    kin = kin_dict[joint]
+    pose = kin.forward(q, end_link=joint)
     pose = np.squeeze(np.asarray(pose))
     pose = [pose[0][3], pose[1][3], pose[2][3]]
     jacobian = kin.jacobian(q).getA()
@@ -20,11 +24,14 @@ def handle_get_robot_pose_jacobian(req):
 
 def get_robot_pose_jacobian_server():
     rospy.init_node('get_robot_pose_jacobian_server')
-    global kin
     robot = URDF.from_parameter_server(key='robot_description')
-    base_link = 'base'
-    end_link = 'right_hand'
-    kin = KDLKinematics(robot, base_link, end_link)
+
+    for joint in joints:
+        kin_dict[joint] = KDLKinematics(robot, 'base', joint)
+
+    global arm
+    arm = ii.Limb('right')
+
     s = rospy.Service('get_robot_pose_jacobian', getRobotPoseAndJacobian, handle_get_robot_pose_jacobian)
     rospy.spin()
 
