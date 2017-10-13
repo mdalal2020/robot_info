ca2ffe4cb315a614b355c76f922815a71affc586
¨Murtaza <mdalal@berkeley.edu>
2017-10-13 16:20:02 -0700

    sawyer server updates

--- scripts/sawyer_server.py
+++ scripts/sawyer_server.py
@@ -7,32 +7,48 @@ from urdf_parser_py.urdf import URDF
 import intera_interface as ii
 from pykdl_utils.kdl_kinematics import KDLKinematics
 
-kin_dict = {}
-arms_dict = {}
+joint_names = [
+    '_l2',
+    '_l3',
+    '_l4',
+    '_l5',
+    '_l6',
+    '_hand'
+]
+
 
 def handle_get_robot_pose_jacobian(req):
-    joint = req.name + req.tip
-    kin = kin_dict[req.name]
-    arm = arms_dict[req.name]
+    poses = []
+    jacobians = []
     q = arm.joint_angles()
     q = [q[req.name + '_j0'], q[req.name + '_j1'], q[req.name + '_j2'], q[req.name + '_j3'], q[req.name + '_j4'],
          q[req.name + '_j5'], q[req.name + '_j6']]
-    pose = kin.forward(q, joint)
-    pose = np.squeeze(np.asarray(pose))
-    pose = [pose[0][3], pose[1][3], pose[2][3]]
-    jacobian = kin.jacobian(q, pose).getA()
-    return getRobotPoseAndJacobianResponse(pose, jacobian[0], jacobian[1], jacobian[2], jacobian[3], jacobian[4], jacobian[5])
+    for joint in joint_names:
+        joint = req.name + joint
+        pose = kin.forward(q, joint)
+        pose = np.squeeze(np.asarray(pose))
+        pose = [pose[0][3], pose[1][3], pose[2][3]]
+        poses.extend(pose)
+        jacobian = kin.jacobian(q, pose).getA()
+        for i in range(3):
+            jacobians.extend(jacobian[i])
+    return getRobotPoseAndJacobianResponse(poses, jacobians)
+
 
 def get_robot_pose_jacobian_server():
     rospy.init_node('get_robot_pose_jacobian_server')
     robot = URDF.from_parameter_server(key='robot_description')
 
-    kin_dict['right'] = KDLKinematics(robot, 'base', 'right_hand')
+    global kin
+    global arm
 
-    arms_dict['right'] = ii.Limb('right')
+    kin = KDLKinematics(robot, 'base', 'right_hand')
+
+    arm = ii.Limb('right')
 
     s = rospy.Service('get_robot_pose_jacobian', getRobotPoseAndJacobian, handle_get_robot_pose_jacobian)
     rospy.spin()
+
 
 if __name__ == "__main__":
     get_robot_pose_jacobian_server()
